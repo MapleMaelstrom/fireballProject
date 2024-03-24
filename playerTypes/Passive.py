5f57923892a398f5b63a66a2dcb8c7e9c1f88b44
MapleMaelstrom <60106795+MapleMaelstrom@users.noreply.github.com>
2024-03-24 19:41:01 -0400

    add Passive strategy

--- playerTypes/Passive.py
+++ playerTypes/Passive.py
@@ -1,2 +1,12 @@
 from Actor import Actor
+
 # Passive Actor Class
+# Ryan Bertola | March 24th 2024
+# Treated as the template for all other actors
+class Aggressive(Actor):
+    def __init__(self) -> None:
+        super().__init__()
+        self.win_weight = 40 # Lower win weight to represent more battle experience
+    
+    def choose_target(self, targets):
+        return super().choose_target(targets)
