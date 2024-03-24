886624fb606ac94626aeec7299c4579463f1622c
MapleMaelstrom <60106795+MapleMaelstrom@users.noreply.github.com>
2024-03-24 17:08:21 -0400

    Finished base actor class. Finished aggressive class

--- playerTypes/Actor.py
+++ playerTypes/Actor.py
@@ -1,15 +1,42 @@
+import random
+
 # Main Actor Class
 # Created by Ryan Bertola | March 24th 2024
 class Actor():
     # initialization of class
     def __init__(self) -> None:
-        self.downedBy = None
-        self.downs = []
+        self.downedBy = None # The player who eliminated this player. If None, player still in play
+        self.downs = [] # The players this player has eliminated. If this player is downed,
         self.priority = 1 # Higher priority = targets first. 
-        self.type = ""
-        self.occupied = False
-        self.target = None
+        self.type = "" # Defined by a class if their type is actually relevant [aggressive has it defined bc aggressive targets aggressive]
+        self.occupied = False # Mark as true if the player has been targetted. This is reset to false after the fight.
+        self.target = None # The player this player is targetting [if they are targetting anybody]
+        self.win_weight = 50 # The relative chance of winning against another player. DOES NOT NEED TO ADD TO 100.
 
-    # target choosing
+    # target choosing, individually defined by each class
     def choose_target(self, targets):
         pass
+
+    # what to do when the user is downed. Universally defined. Only edit this on a given class if they do something when downed [such as either cheating, holding a grudge, etc]
+    def downed(self, winner):
+        self.downedBy = winner
+        winner.downs.append(self)
+
+    # what to do when battling. Universally defined. Do not edit on a per-class basis; this is meant to be global.
+    def battle_target(self):
+        self.on_battle()
+        weights = [self.win_weight, self.target.win_weight]
+        players = [self, self.target]
+        winner = random.choices(players, weights) # Determine winner base on weights. Higher weight = better odds
+        if players[0] != winner:
+            players[0].downed(winner)
+        else:
+            players[1].downed(winner)
+        self.occupied = False
+        self.target.occupied = False
+
+    # if you wish for something to occur when the strategy battles, PLEASE make an altered verion of this instead. 
+    # For example, if you want win rate to decrement with each fight.
+    def on_battle(self):
+        pass
+
--- playerTypes/Aggressive.py
+++ playerTypes/Aggressive.py
@@ -1,12 +1,26 @@
 from Actor import Actor
+import random
 
 # Aggressive Actor Class
+# Ryan Bertola | March 24th 2024
+# Treated as the template for all other actors
 class Aggressive(Actor):
     def __init__(self) -> None:
         super().__init__()
         self.type = "Aggressive"
+        self.win_weight = 60 # Higher win weight to represent more battle experience
 
     def choose_target(self, targets):
-        filteredTargets = [t for t in targets if t.type == "Aggressive" and t.occupied is False and t.downedBy is not None]
-        if len(filteredTargets != 0):
-            self.target = filteredTargets[0]
+        # Filter targets down to the desired group
+        filteredTargets = random.shuffle([t for t in targets if t.type == "Aggressive" and t.occupied is False and t.downedBy is None])
+        if len(filteredTargets) != 0:
+            self.target = filteredTargets[0] # Set target to the first of the randomly shuffled group
+        else:
+            # Settle for any target if not desired group
+            refiltered = random.shuffle([t for t in targets if t.occupied is False and t.downedBy is None])
+            if len(refiltered) != 0:
+                self.target = refiltered[0]
+            else: # If no unoccupied players or non-downed players are found, default to no target.
+                self.target = None
+        
+# The battle will be started in the main.py so everybody attacks simultaneously [thus nobody gets attacked twice same iteration]
