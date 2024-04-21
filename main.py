2b1720e4cdfb14aeac1f5e1d5529b44058165c9b
MapleMaelstrom <60106795+MapleMaelstrom@users.noreply.github.com>
2024-04-21 03:10:05 -0400

    Implement Random Strategy

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from playerTypes.Aggressive import Aggressive
 from playerTypes.Passive import Passive
+from playerTypes.RandStrat import RandomStrat
 from game import Game
 
-firstList = [Aggressive() for i in range(50)] + [Passive() for j in range(50)]
-firstGame = Game(firstList)
-firstGame.iterate()
+# firstList = [Aggressive() for i in range(50)] + [Passive() for j in range(50)]
+# firstGame = Game(firstList)
+# firstGame.iterate()
--- playerTypes/RandStrat.py
+++ playerTypes/RandStrat.py
@@ -1,2 +1,27 @@
 from playerTypes.Actor import Actor
-# Random-targetting Actor Class
+import random
+
+# RandomStrat Actor Class
+# Ryan Bertola | April 20th 2024
+# A strategy with no strategy. Just random.
+# This class is not named random so as to not interfere with the existent random library.
+class RandomStrat(Actor):
+    def __init__(self) -> None:
+        super().__init__()
+        self.type = "Random"
+    
+    def __str__(self):
+        return "Random"
+
+    def choose_target(self, targets):
+        # Filter targets down to the desired group
+        if self.occupied:
+            self.target = None
+            return
+        filteredTargets = [t for t in targets if t.occupied is False and t.downedBy is None] # Remove occupied and downed players.
+        random.shuffle(filteredTargets)
+        if len(filteredTargets) != 0:
+            self.target = filteredTargets[0] # Set target to the first of the randomly shuffled group
+        else:
+            self.target = None # If nobody is available we don't want them to target
+        
