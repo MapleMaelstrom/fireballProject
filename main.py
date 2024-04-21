6ef20f158f0fbe5aef942b0595230b57c649b37b
MapleMaelstrom <60106795+MapleMaelstrom@users.noreply.github.com>
2024-04-21 03:15:34 -0400

    Implement Winner Targetting Strategy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from playerTypes.Aggressive import Aggressive
 from playerTypes.Passive import Passive
 from playerTypes.RandStrat import RandomStrat
+from playerTypes.WinnerTargetting import WinTarg
 from game import Game
 
 # firstList = [Aggressive() for i in range(50)] + [Passive() for j in range(50)]
--- playerTypes/WinnerTargetting.py
+++ playerTypes/WinnerTargetting.py
@@ -1,2 +1,26 @@
 from playerTypes.Actor import Actor
-# Winner-targetting Actor Class
+import random
+
+# WinTarg Actor Class
+# Ryan Bertola | March 24th 2024
+# Will target exclusively those they deem the winner. Only activates when somebody has >4 downs.
+class WinTarg(Actor):
+    def __init__(self) -> None:
+        super().__init__()
+        self.type = "Winner Targetting"
+        self.priority = 2 # Higher priority means the winner doesn't get a chance to target somebody else.
+    
+    def __str__(self):
+        return "Winner Targetting"
+
+    def choose_target(self, targets):
+        # Filter targets down to the desired group
+        if self.occupied:
+            self.target = None
+            return
+        filteredTargets = [t for t in targets if t.occupied is False and t.downedBy is None] # Removes downed / occupied players.
+        filteredTargets.sort(key=lambda x: len(x.downs), reverse=True) # Sorts by downs in descending order
+        if len(filteredTargets) != 0 and len(filteredTargets[0].downs) >= 5: # Ensures 5 downs.
+            self.target = filteredTargets[0] # Set target to the leader
+        else:
+            self.target = None # Does not target anyone otherwise.
